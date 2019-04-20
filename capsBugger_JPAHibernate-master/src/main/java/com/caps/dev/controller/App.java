600ac03ff5630903b650bf6c51a9632a6cd55109
Basudevghosh <basudev.96jsr@gmail.com>
2019-04-20 14:02:30 +0530

    Add files via upload

---
+++ capsBugger_JPAHibernate-master/src/main/java/com/caps/dev/controller/App.java
@@ -0,0 +1,52 @@
+package com.caps.dev.controller;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import com.caps.dev.beans.Dog;
+import com.caps.dev.dao.DogDAO;
+import com.caps.dev.dao.DogJPAImpl;
+import com.caps.dev.one.to.one.Address;
+import com.caps.dev.one.to.one.Person_OneToOneUnidirectional;
+import com.caps.dev.util.JPAUtils;
+
+public class App {
+	public static void main(String[] args) {
+		
+		Person_OneToOneUnidirectional p = 
+				new Person_OneToOneUnidirectional();
+		p.setName("Jayaprakash");
+		p.setAge(21);
+		p.setId(1);
+		
+		Address address = new Address();
+		address.setCity("Tirupathi");
+		address.setPin(517370);
+		address.setState("AP");
+		address.setAddress_id(101);
+		p.setAddress(address);
+		
+		EntityManagerFactory emf = JPAUtils.getEntityManagerFactory();
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+//		em.persist(p);
+		p = em.find(Person_OneToOneUnidirectional.class, 1);
+		em.remove(p);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
